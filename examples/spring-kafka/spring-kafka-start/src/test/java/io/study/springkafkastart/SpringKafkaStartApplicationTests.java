be202aa23e35f4a36be33e752101d2e07dc96e8e
soon.good <playmymind@daum.net>
2021-09-25 18:15:45 +0900

    초기파일 커밋

---
+++ examples/spring-kafka/spring-kafka-start/src/test/java/io/study/springkafkastart/SpringKafkaStartApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.springkafkastart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
